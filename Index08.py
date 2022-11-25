8ab3d138b0c0dbccc87daa4ba13beaf6841ed970
Javohir427 <ortiqxojayevjavohir0511@gmail.com>
2022-11-26 02:31:20 +0500

    08

--- Index08.py
+++ Index08.py
@@ -6,5 +6,10 @@ def main(s):
     Returns:
         int: answer
     """
-    return
+    if s.index('*'):
+        return s.index('*')
+    else :
+        return False
+
+print(main("good"))
         
